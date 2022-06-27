6003fc0793b36873009ccc91c0827b59062f6ac6
unknown <peluffojuancruz@gmail.com>
2022-06-27 18:51:35 -0300

    Database connection. Models and associations

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,28 @@
+const { UUIDV4 } = require('sequelize');
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('videogame', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            defaultValue: UUIDV4
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        released: {
+            type: DataTypes.DATEONLY
+        },
+        rating: {
+            type: DataTypes.DECIMAL(4, 2)
+        }
+    });
 };
